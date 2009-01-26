0128c62d334bb0b81a66a92ad8bddec9472e3c50
Björn Mellström <bjorn.mellstrom@purplescout.se>
2009-01-26 13:50:32 +0100

    Added stub implementation for non-Linux platforms

---
+++ processtitle.py
@@ -0,0 +1,28 @@
+# encoding: utf-8
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=120:
+# Respect whitespace settings!
+"""
+
+Copyright (c) 2009 Purple Scout. All rights reserved.
+
+"""
+
+# Stub implementation for other platforms than Linux
+
+def gettitlemaxlen():
+    return 0
+
+def gettitle():
+    return ""
+
+def settitle(title):
+    pass
+
+def getkilltitlemaxlen():
+    return 0
+
+def getkilltitle():
+    return ""
+
+def setkilltitle(title):
+    pass
--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
 from setuptools import distutils
 from distutils.core import setup, Extension
+import sys
 
-module1 = Extension('processtitle',
-                    sources = ['processtitle.c'])
+name = 'ProcessTitle'
+version = '0.0.1'
+description = 'Module for setting process titles'
+author = 'Purple Scout AB'
 
-setup (name = 'ProcessTitle',
-       version = '0.0.1',
-       description = 'Module for setting process titles',
-       author = 'Purple Scout AB',
-       ext_modules = [module1],
-       zip_safe = False)
+if sys.platform == "linux2":
+    module1 = Extension('processtitle',
+                        sources = ['processtitle.c'])
+
+    setup(name = name,
+          version = version,
+          description = description,
+          author = author,
+          ext_modules = [module1],
+          zip_safe = False)
+
+else:
+    setup(name = name,
+          version = version,
+          description = description,
+          author = author,
+          py_modules = ['processtitle'],
+          zip_safe = True)
