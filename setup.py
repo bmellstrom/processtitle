78424d9861987e7b0e11bcbeab2a6bd23de443f0
Björn Mellström <bjorn.mellstrom@purplescout.se>
2009-01-27 11:47:06 +0100

    Added standard debian package

--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
-from setuptools import distutils
 from distutils.core import setup, Extension
 import sys
 
-name = 'ProcessTitle'
 version = '0.0.1'
-description = 'Module for setting process titles'
 author = 'Purple Scout AB'
+description = 'Module for setting process titles'
+author_email = "info@purplescout.se"
 
 if sys.platform == "linux2":
-    module1 = Extension('processtitle',
-                        sources = ['processtitle.c'])
+    name = "processtitle"
+    processtitle = Extension('processtitle', sources = ['processtitle.c'])
 
     setup(name = name,
           version = version,
-          description = description,
           author = author,
-          ext_modules = [module1],
-          zip_safe = False)
+          author_email = author_email,
+          url = "http://www.purplescout.se",
+          description = description,
+          ext_modules = [processtitle])
 
 else:
+    name = 'ProcessTitle'
+
     setup(name = name,
           version = version,
           description = description,
